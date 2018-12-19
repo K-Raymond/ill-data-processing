6a5593d6ebe324811fcd45f820e9f9504dfc5af8
Kurtis Raymond <kraymond@sfu.ca>
2018-12-18 20:51:35 -0800

    checkpoint commit

--- ConstructGG.C
+++ ConstructGG.C
@@ -1,5 +1,5 @@
 #include<vector>
-#include<string>
+#include <string>
 
 #include "TH2D.h"
 #include "TTreeReader.h"
@@ -8,47 +8,88 @@
 #include "TFileCollection.h"
 #include "TCollection.h"
 #include "TChain.h"
+#include "TROOT.h"
 #include "THashList.h"
+#include "TThread.h"
 
 #include "./detCal.h"
+#include "./progressThread.h"
+
+static long long int TotalEntries;
+static long long int PartialEntries;
+static bool GO = true;
+
+static void* UpdateProgress(void *ptr)
+{
+    while( GO == true )
+    {
+        TThread::Lock();
+        printf("\rProgress %.2f%%, %lld of %lld", 
+                100*PartialEntries/(double)TotalEntries,
+                PartialEntries,
+                TotalEntries);
+        TThread::UnLock();
+
+        // sleep in milliseconds
+        gSystem->Sleep(20);
+    }
+    return NULL;
+}
 
 TH2D* ConstructGG( TFileCollection* fc )
 {
-    //gSystem->Load("./libdetCal.so");
     detCal* Channel = new detCal("./XPConfig.txt");
+
+    printf("Progress %.2f%%, %lld of %lld", 
+            100*PartialEntries/(double)TotalEntries,
+            PartialEntries,
+            TotalEntries);
+    TThread *t1 = new TThread("t1", UpdateProgress, NULL);
+    t1->Run();
+
+    // Load Lst2RootTree's into chain and reader
+    TChain* pChain = new TChain("Lst2RootTree");
+    pChain->AddFileInfoList( fc->GetList() );
+
+    // Event Parameters
+    TTreeReader TreeR(pChain);
+    TotalEntries = TreeR.GetEntries(true);
+    TTreeReaderArray<int32_t> energy(TreeR, "energy");
+    TTreeReaderArray<int16_t> adc(TreeR, "adc");
+    TTreeReaderValue<int> multiplicity(TreeR, "multiplicity");
 
     TH2D* ggMat = new TH2D("ggMat", "Gamma Gamma Coincidence",
             8191, 0, 12000,
             8191, 0, 12000);
 
-    // Load Lst2RootTree's into chain
-    TChain* pChain = new TChain("Lst2RootTree");
-    pChain->AddFileInfoList( fc->GetList() );
-
-    TTreeReader TreeR(pChain);
-    TTreeReaderArray<int32_t> energy(TreeR, "energy");
-    TTreeReaderArray<int16_t> adc(TreeR, "adc");
-    TTreeReaderValue<int> multiplicity(TreeR, "multiplicity");
-
     bool vito = false;
     int eventMulti;
-    // Array for holding all energies in coincidence
-    
-    printf("Constructing gamma gamma matrix\n");
+     
+    //  Begin gg construction
     while ( TreeR.Next() ) {
-        // Find the size of the packet
+        // Reset for next group
         vito = false;
         eventMulti = *multiplicity;
+
+        //while(TThread::TryLock()) {}
+        PartialEntries++;
+        //TThread::UnLock();
+
+        //** Descriminators **//
         if( eventMulti == 1 ) // if == 1 skip
-            continue;
-        
+            continue;        
         for( int i = 0; i < eventMulti; i++ )
         {
            if( Channel->isVito( adc[i] ) )
                vito = true;
         }
+        for( int i = 0; i < eventMulti; i++ )
+        {
+            if( energy[i] < 2 )
+                vito = true;
+        }
 
-        // Check if result is valid
+        // fill results if valid
         if( !vito )
             for( int i = 0; i < eventMulti; i++ )
                 for( int j = i+1; j < eventMulti; j++ )
@@ -58,13 +99,31 @@ TH2D* ConstructGG( TFileCollection* fc )
                             Channel->GetEnergy( energy[j], adc[j] ));
 
     }
+
+    GO = false;
+    printf("\rProgress %.2f%%, %lld of %lld\n", 
+            100*PartialEntries/(double)TotalEntries,
+            PartialEntries,
+            TotalEntries);
     delete Channel;
     delete pChain;
     return ggMat;
 }
+
 
 TH2D* ConstructGG( std::string TFileList )
 {
     TFileCollection* fc = new TFileCollection( "RootFileList", "", TFileList.c_str() );
     return ConstructGG( fc );
 }
+
+/*
+TH2D* ConstructGGParallel( std::string TFileList )
+{
+    TFileCollection* fc = new TFileCollection( "RootFileList", "", TFileList.c_str() );
+    ROOT::EnableThreadSafety();
+
+    ROOT::MakeThreaded<TH2F> ParHisto("ParHisto", "gg", 8191, 0, 12000, 8191, 0, 12000);
+    ROOT::TTreeProcessor
+
+*/
--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
+all: detCal progressThread
 
 detCal:
 	genreflex detCal.h --rootmap detCal.rootmap
 	g++ -std=c++11 -shared -fPIC -o libdetCal.so detCal.cpp detCal_rflx.cpp `root-config --cflags --libs`
+
+progressThread:
+	genreflex progressThread.h --rootmap progressThread.rootmap
+	g++ -std=c++11 -fopenmp -shared -fPIC -o libprogressThread.so progressThread.cpp progressThread_rflx.cpp `root-config --cflags --libs`
 
 clean:
 	rm libdetCal.so
